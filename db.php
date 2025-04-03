d01684dc3eccd6361dc58835f89e96bf7674805a
akhita13 <asajeev23it@student.mes.ac.in>
2025-04-03 11:37:04 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "login_system");
+$conn = mysqli_connect("localhost", "root", "", "login_system");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
